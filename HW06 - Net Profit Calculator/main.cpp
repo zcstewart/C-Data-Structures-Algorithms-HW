5f46db51fa7e611c35e66219488b508d4a1e2438
Zach Stewart <zcstewart.90@gmail.com>
2023-01-29 15:33:53 -0800

    Improved variable names

--- HW06 - Net Profit Calculator/main.cpp
+++ HW06 - Net Profit Calculator/main.cpp
@@ -23,8 +23,8 @@
  *                      profit is 10% of the initial merchandise cost.
  * 
  *                      The desired profit percentage and sale percentage 
- *                      are constant double variables named "percProfit" and
- *                      "percSale", respectively.
+ *                      are constant double variables named "percentProfit" and
+ *                      "percentSale", respectively.
  * 
  *  Author:             Zachary Charles Stewart
  *  Student ID:         105903414
@@ -42,61 +42,61 @@ int main()
 {
     //Global constants for profit percentage and sale percentage
     //Also included is the markup percentage (too be determined)
-    const double percProfit = 10.0; //Desired profit percentage
-    const double percSale = 15.0;   //Desired sale price
+    const double percentProfit = 10.0; //Desired profit percentage
+    const double percentSale = 15.0;   //Desired sale price
     
     
     //Declare variables for program
-    double costMerch = 0;       //Cost of merchandise
-    double costEmp = 0;         //Cost of employee salaries
-    double costRent = 0;        //Cost of rent
-    double costElec = 0;        //Cost of electricity
-    double costOp = 0;          //Operating cost
-    double priceMU = 0;         //Price after applied markup
-    double priceSale = 0;       //Price after sale   
-    double profitNet = 0;       //Profit after merchandise sold on sale
-    double amountMU = 0;        //Amount of markup
+    double costMerchandise = 0;         //Cost of merchandise
+    double costLabor = 0;               //Cost of employee salaries
+    double costRent = 0;                //Cost of rent
+    double costElectricity = 0;         //Cost of electricity
+    double costTotalOperating = 0;      //Operating cost
+    double priceMarkup = 0;             //Price after applied markup
+    double priceSale = 0;               //Price after sale   
+    double profitNet = 0;               //Profit after merchandise sold on sale
+    double amountMarkup = 0;            //Amount of markup
     
     //Declare "junk" char variable to read in '$'
     char junk;
     
     //Prompt user for input
-    cout << "Please enter the total cost of the merchandise: " << endl;
-    cin >> junk >> costMerch;
-    cout << "Please enter the total cost of all employee salaries: " << endl;
-    cin >> junk >> costEmp;
-    cout << "Please enter the yearly rent: " << endl;
+    cout << "Please enter the total cost of the merchandise (including $): " << endl;
+    cin >> junk >> costMerchandise;
+    cout << "Please enter the total cost of all employee salaries (including $): " << endl;
+    cin >> junk >> costLabor;
+    cout << "Please enter the yearly rent (including $): " << endl;
     cin >> junk >> costRent;
-    cout << "Please enter the estimated electricity costs: " << endl;
-    cin >> junk >> costElec;
+    cout << "Please enter the estimated electricity costs (including $): " << endl;
+    cin >> junk >> costElectricity;
     cout << endl << endl;
     
     //Determine total operating costs
-    costOp = costMerch + costEmp + costRent + costElec;
+    costTotalOperating = costMerchandise + costLabor + costRent + costElectricity;
     
     //Determine desired net profit (percentage from operating costs)
-    profitNet = (percProfit/100) * costMerch;
+    profitNet = (percentProfit/100) * costMerchandise;
      //Determine sale price
-    priceSale = profitNet + costOp;
+    priceSale = profitNet + costTotalOperating;
     
-    //Determine markup price (0.85*priceMU = priceSale)
-    priceMU = priceSale * (100.0 / (100.0 - percSale));
+    //Determine markup price (0.85*priceMarkup = priceSale)
+    priceMarkup = priceSale * (100.0 / (100.0 - percentSale));
     
     //Determine total markup amount
-    amountMU = priceMU - costMerch;
+    amountMarkup = priceMarkup - costMerchandise;
     
  
     //Output Statements
     cout << "Merchandise should be sold at $" << setprecision(2) << fixed 
-         << showpoint << priceMU << "." << endl << endl;
+         << showpoint << priceMarkup << "." << endl << endl;
     cout << "With a 15% sale, total revenues are $" << priceSale 
          << setprecision(2) << fixed << showpoint << endl << endl;
     cout << "After operating costs, the net profit is $" << profitNet
          << setprecision(2) << fixed << showpoint << "." << endl << endl;
     cout << "The net profit is calculated to be " << setprecision(1) << fixed
-         << showpoint << percProfit << "%" << endl;
+         << showpoint << percentProfit << "%" << endl;
     cout << "of the initial $" << setprecision(2) << fixed << showpoint 
-         << costMerch << " merchandise cost." << endl;
+         << costMerchandise << " merchandise cost." << endl;
     
     //Terminate program upon successful completion
     exit(EXIT_SUCCESS);
